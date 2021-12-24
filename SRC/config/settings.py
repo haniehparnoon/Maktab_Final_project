424c28465f3ac327512644b0ad1a3010f5bd7ea2
hanieh parnoon <hanieh.satir@gmail.com>
2021-12-25 01:26:45 +0330

    update settings.py to use postgresql

--- SRC/config/settings.py
+++ SRC/config/settings.py
@@ -72,16 +72,20 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = 'postgresqltest.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'online_order_food', 
+        'USER': 'postgres', 
+        'PASSWORD': '5560',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 

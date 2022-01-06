cb3709189cb16616cd36237229b9de06080dfebf
hanieh parnoon <hanieh.satir@gmail.com>
2022-01-06 13:32:39 +0330

    add admin page

--- SRC/restaurant/views.py
+++ SRC/restaurant/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.http import request
+from django.shortcuts import render,  redirect
+from django.views.generic import TemplateView,ListView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
+from .models import *
+from accounts.models import *
+from .decorators import superuser_required, is_staff_required, customer_required
+from django.db.models.aggregates import Count, Sum
+from .forms import FoodForm, CategoryForm
+import jdatetime
 
 # Create your views here.
-class HomePage(TemplateView):
-    template_name = 'home.html'
+@superuser_required()
+class AdminHome(CreateView):
+    model = Food
+    template_name = 'restaurant\home_admin.html'
+    form_class = FoodForm
+    success_url = reverse_lazy('Foods')
 

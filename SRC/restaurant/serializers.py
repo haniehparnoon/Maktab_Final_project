4c9bab737e6464f0bbd2613517e2f09f6f40b99a
hanieh parnoon <hanieh.satir@gmail.com>
2022-01-13 15:02:45 +0330

    add serializers.py

---
+++ SRC/restaurant/serializers.py
@@ -0,0 +1,22 @@
+from .models import *
+from rest_framework import serializers
+
+class BranchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
+        fields = ['id', 'name']
+
+class FoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Food
+        fields = ['id', 'name'] 
+
+class MenuSerializer(serializers.ModelSerializer):
+    food = FoodSerializer
+    class Meta:
+        model = Menu
+        fields = ['food','branch'] 
+
+
+        
+

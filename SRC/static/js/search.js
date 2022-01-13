96bb7bba92423336fc4cf4796ef21686216afaab
hanieh parnoon <hanieh.satir@gmail.com>
2022-01-13 15:03:35 +0330

    add sear.js file

---
+++ SRC/static/js/search.js
@@ -0,0 +1,79 @@
+$( document ).ready(function() {
+
+    // {{test}} error
+    // console.log(test) error
+    console.log(URL) 
+   
+    
+    
+    $(".formsearch").on("click", function() {
+       
+        send_ajax($('#text_search').val())
+    });
+        function send_ajax(input_data){
+            data={
+                'csrfmiddlewaretoken':CSRF_TOKEN,
+                'search_input':input_data
+                };
+                console.log(data)
+            $.ajax({
+                type: 'POST',
+                url: URL,
+                dataType: 'json',
+                data:data,
+                success: function(res) {
+                    console.log(res);
+                    show_branches(res)
+                }
+            });
+        }
+        
+        function show_branches(data){
+            branch_ul_tag= $('#branch_ul')
+            food_ul_tag= $('#food_ul')
+            branch_ul_tag.empty()
+            food_ul_tag.empty()
+           
+            var branch = data.branches;
+            var food = data.foods ;    
+           
+            if ( branch ){
+              
+                $.each(branch, function(i, branch){
+                    var li = document.createElement("li");
+                    // var span = document.createElement("span");
+                    var a = document.createElement("a");
+                    // Create the text node for anchor element.
+                    var link = document.createTextNode(branch.name);
+                  
+                // Append the text node to anchor element.
+                    a.appendChild(link); 
+                    
+                    a.href = "{% url 'menurestaurant' branch.id %}"; 
+                    li.append(a)
+                    branch_ul_tag.append(li)
+                   
+                });
+               
+                
+            }else{
+                branch_ul_tag.append()
+            }
+            if ( food ){
+                $.each(food, function(i, food){
+                    var li = document.createElement("li");
+                    var span = document.createElement("span");
+                    span.append(food.name) 
+                    li.append(span)
+                    
+                    food_ul_tag.append(li)
+                   
+                });
+               
+                
+            }else{
+                food_ul_tag.append()
+            }
+            
+        }
+      });
